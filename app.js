f035c685273e5fd7178e22370a13f67bd48ffa39
Hekey <87144005+Hkp102004@users.noreply.github.com>
2025-11-16 23:22:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,130 @@
+let currentMusic = 0;
+
+const music = document.querySelector('#audio');
+const seekbar = document.querySelector('.seek-bar');
+const playbtn = document.getElementById('play');
+const pausebtn = document.getElementById('pause');
+const nexttbn = document.getElementById('next');
+const prevbtn = document.getElementById('back');
+const songname = document.querySelector('.name');
+
+console.log("Yes I'm working properly without any bugs");
+
+// function songplay() {
+//     if(playbtn.style.opacity == "1"){
+//         playbtn.style.opacity = "0";
+//         pausebtn.style.opacity = "1";
+//         playbtn.style.pointerEvents = "none";
+//         pausebtn.style.pointerEvents = "auto";
+//         music.play();
+//     }
+//     else{
+//         playbtn.style.opacity = "1";
+//         pausebtn.style.opacity = "0";
+//         playbtn.style.pointerEvents = "auto";
+//         pausebtn.style.pointerEvents = "none";
+//         music.pause();
+//     }
+// }
+playbtn.addEventListener("click", ()=>{
+    if(music.paused)
+    {
+        audio.play();
+        playbtn.src="photos/pause.png";
+    }
+    else
+    {
+        music.pause();
+        playbtn.src="photos/play-button.png";
+    }
+})
+
+const setMusic = (i) => {
+    seekbar.value = 0;
+    let song = songs[i];
+    cureentMusic = i;
+    music.src = song.path;
+    songname.innerHTML = song.name;
+    seekbar.max = music.duration;
+}
+
+setMusic(0);
+
+// setInterval(() => {
+//     seekbar.value = music.currentTime;
+// },500);
+
+
+// seekbar.addEventListener('change', () => {
+//     music.currentTime = seekbar.value;
+// });
+
+music.addEventListener("timeupdate", ()=>{
+    let progress = (music.currentTime/music.duration)*100;
+    seekbar.value=progress;
+});
+
+seekbar.addEventListener("input",()=>{
+    music.currentTime = (seekbar.value/100)*music.duration;
+});
+
+
+
+const playMusic = () => {
+    music.play();
+    playbtn.src="photos/play-button.png";
+    // playbtn.style.opacity = "0";
+    // pausebtn.style.opacity = "1";
+    // playbtn.style.pointerEvents = "none";
+    // pausebtn.style.pointerEvents = "auto";
+}
+
+nexttbn.addEventListener('click', () => {
+    if(currentMusic >= songs.length - 1){
+        currentMusic = 0;
+    }else{
+        currentMusic++;
+    }
+    setMusic(currentMusic);
+    playMusic();
+    playbtn.src="photos/pause.png";
+})
+
+prevbtn.addEventListener('click', () => {
+    if(currentMusic <= 0){
+        currentMusic = songs.length - 1;
+    }else{
+        currentMusic--;
+    }
+    setMusic(currentMusic);
+    playMusic();
+    playbtn.src="photos/pause.png";
+})
+
+music.addEventListener("ended",()=>{
+    currentMusic++;
+    if(currentMusic >= music.length)
+    {
+        currentMusic=0;
+    }
+    setMusic(currentMusic); //this line is the main reason for next song, remeber it at all cost
+    playMusic();
+    playbtn.src="photos/pause.png";
+})
+
+const { app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+  const win = new BrowserWindow({
+    width: 300,
+    height: 150,
+    frame: false, // no title bar
+    transparent: true, // makes background invisible
+    alwaysOnTop: true,
+  });
+
+  win.loadFile('music_player.html');
+});
+
+
+console.log("Yes everything is still working");
---
+++ data.js
@@ -0,0 +1,98 @@
+let songs = [
+    {
+        name : 'Chammak Challo',
+        path: 'songs/chammak challo.mp3',
+    },
+    {
+        name : 'Amazing spider man',
+        path: 'songs/TASM theme.mp3',
+    },
+    {
+        name : '1234 not gonna type anymore',
+        path: 'songs/1234 get on the dance floor.mp3',
+    },
+    {
+        name : 'Alive',
+        path: 'songs/alive.mp3',
+    },
+    {
+        name : 'Bheegi si Bhagi si',
+        path: 'songs/Bheggi si.mp3',
+    },
+    {
+        name : 'Biba',
+        path : 'songs/biba.mp3',
+    },
+    {
+        name:'Dandeliona',
+        path:'songs/dandelions.mp3',
+    },
+    {
+        name:'Ek main aur ek tu',
+        path:'songs/ek main aur ek tu.mp3',
+    },
+    {
+        name:'Naruto OP 7',
+        path:'songs/jiraya.mp3',
+    },
+    {
+        name:'Khuda Jaane',
+        path:'songs/khuda jaane.mp3',
+    },
+    {
+        name:'Killshot',
+        path:'songs/killshot.mp3',
+    },
+    {
+        name:'Kushina Theme',
+        path:'songs/kushina.mp3',
+    },
+    {
+        name:'My Home Naruto',
+        path:'songs/my home.mp3',
+    },
+    {
+        name:'Naruto Theme song special',
+        path:'songs/naruto.mp3',
+    },
+    {
+        name:'New Jeans',
+        path:'songs/New jeans.mp3',
+    },
+    {
+        name:'One Kiss',
+        path:'songs/one kiss.mp3',
+    },
+    {
+        name:'Pink Lips',
+        path:'songs/pink lips.mp3',
+    },
+    {
+        name:'Shield Hero OST',
+        path:'songs/Shield hero.mp3',
+    },
+    {
+        name:'Solo Leveling',
+        path:'songs/solo leveling.mp3',
+    },
+    {
+        name:'Tera Hone Laga Hun',
+        path:'songs/tera hone laga hun.mp3',
+    },
+    {
+        name:'Tip Toes',
+        path:'songs/tip toes.mp3',
+    },
+    {
+        name:'Titli',
+        path:'songs/titli.mp3',
+    },
+    {
+        name:'Tu Jaane Na',
+        path:'songs/tu jaane na.mp3',
+    },
+    {
+        name:"We don't talk anymore",
+        path:"songs/we don't talk anymore.mp3",
+    },
+]
